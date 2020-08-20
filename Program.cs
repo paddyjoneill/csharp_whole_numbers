c397d6ca978c9aa94348b58d703d2c11fc875c79
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-08-20 18:28:46 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace whole_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decimalNotation = 2_000_000;
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
+
+            Console.WriteLine($"{decimalNotation == binaryNotation}");
+            Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+        }
+    }
+}
